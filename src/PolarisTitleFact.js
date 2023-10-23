a99f27cff36045a583fd3a71902de309b5b3f476
wasta <ccw5294@psu.edu>
2023-10-23 00:17:33 -0400

    define cards and start the grid

--- src/PolarisTitleFact.js
+++ src/PolarisTitleFact.js
@@ -10,17 +10,76 @@ export class PolarisTitleFact extends LitElement {
   static get styles() {
     return css`
       :host {
-        display: block;
+        
+      }
+      .card-wrap-outer {
+        margin-top: 2rem;
+        gap: 16px;
+        display: flex;
+        flex-wrap: wrap; 
+        width: 100%;
+      }
+
+      .blue-card {
+        background-color: #1e407c;
+        box-shadow: 0 8px 16px 0 rgba(0,3,33,.1);
+        color: #fff;
+        align-items: center;
+        display: grid;
+        justify-content: center;
+      }
+
+      .white-card {
+        background-color: #fff;
+        box-shadow: 0 8px 16px 0 rgba(0,3,33,.1);
+        color: #1e407c;
+        align-items: center;
+        display: grid;
+        justify-content: center;
+      }
+
+      .blue-card-fade {
+        background: linear-gradient(180deg, rgba(30,64,124,1) 0%, rgba(0,30,68,1) 65%, rgba(0,30,68,1) 100%);
+        box-shadow: 0 8px 16px 0 rgba(0,3,33,.1);
+        color: white;
+        align-items: center;
+        display: grid;
+        justify-content: center;
+      }
+
+      @media (min-width: 768px) {
+        .card-wrap-outer {
+          height: 36vw;
+          flex: 0 0 48%;
+        }
+
+      @media (min-width: 500px) {
+        .card-wrap-outer {
+          height: 44vw;
+          flex: 0 0 48%;
+        }
+      }
+
+      @media (min-width: 1080px) {
+        .card-wrap-outer{
+          height: 25vw;
+          flex: 0 0 48%;
+        }
       }
     `;
   }
 
   constructor() {
     super();
-    this.title = 'My boilerplate';
+    this.blueTitle = 'Power Facts';
   }
 
   render() {
-    return html`<span>${this.title}</span>`;
+    return html`
+    <div class="card-wrap-outer">
+      <div class="blue-card">${this.blueTitle}</div>
+      <div class="white-card">White Card</div>
+      <div class="blue-card-fade">Fade</div>
+    </div>`;
   }
 }
